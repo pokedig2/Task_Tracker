221e5ad6a000c4f0a9559b202b400110e07b666b
Destin <destin4235@gmail.com>
2025-01-08 13:22:00 -0800

    Initial commit to ensure project is being pushed

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Testing that GitHub is working")
+# Goal of project is to create a fully functional task tracker - Destin
+# Check for JSON file, if does not exist generate new JSON file
+list = open("tasks.json", "w")
+data = {
+
+}
+
+#create perpetual running to send commands to list application
+run = True
+while run:
+    temp = input()
+    if temp == "exit":
+        print("Exiting...")
+        run = False
